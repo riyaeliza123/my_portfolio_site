1fcc4642f0f76916857c970b4e6bf22570f2e781
RIYA ELIZA <riya2023@ubc.student.ca>
2023-10-21 09:53:59 -0700

    pushing json final

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Riya Eliza Shaju",
+  title: "Data Science Facilitator",
+  email: "riyaelizashaju@gmail.com",
+  gitHub: "https://github.com/riyaeliza123",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/riya-eliza-shaju/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
